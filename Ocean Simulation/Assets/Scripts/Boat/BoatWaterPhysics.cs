c0b21ceb244ff90e48ba8bfc91b05f1c91d871e5
Shan Memon <shankmemon@gmail.com>
2021-05-01 00:02:16 -0500

    Got ship in water

---
+++ Ocean Simulation/Assets/Scripts/Boat/BoatWaterPhysics.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BoatWaterPhysics : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+    public int floaterCount = 1;
+    public float waterDrag = 0.99f;
+    public float waterAngularDrag = 0.5f;
+
+
+    private void FixedUpdate() 
+    {
+        rigidbody.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+
+        float waveHeight = WaterController.current.getHeightAtPosition(transform.position);
+        if (transform.position.y < waveHeight)
+        {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            rigidbody.AddForce(displacementMultiplier * -rigidbody.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rigidbody.AddTorque(displacementMultiplier * -rigidbody.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+        }
+    }
+}
